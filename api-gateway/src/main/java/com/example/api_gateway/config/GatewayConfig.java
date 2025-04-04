7e8b6dd20219f2b9cd9b310cc455f15bc5b8a772
11qfour <futurenature31@gmail.com>
2025-04-04 18:42:13 +0300

    add api-gateway

---
+++ api-gateway/src/main/java/com/example/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,23 @@
+package com.example.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.RouteLocator;
+
+@Configuration
+public class GatewayConfig {
+    /*@Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r
+                        .path("/users/**")
+                        .uri("lb://USER-SERVICE"))
+
+                .route("company-service", r -> r
+                        .path("/companies/**")
+                        .uri("lb://COMPANY-SERVICE"))
+
+                .build();
+    }*/
+}
